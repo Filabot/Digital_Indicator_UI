3aa8ce45a31e5655c82b199caff5a3658e74f89a
coolio986 <coolio986@hotmail.com>
2019-12-04 17:32:06 -0600

    Initial Commit

---
+++ Digital_Indicator.Startup/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace Digital_Indicator.Startup.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Filalogger - Filament Diameter Measurement";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
